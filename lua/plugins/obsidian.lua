7464df2753ae51502a7a23a149405cd2fe56eeb0
n.pujol-mendez <n.pujol-mendez@mobilistics.de>
2025-04-28 14:13:16 +0200

    [ADDED] obsidian config

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,34 +1,37 @@
+local function workspace_exists(path)
+  if path:sub(1, 1) == "~" then
+    path = vim.fn.expand(path)
+  end
+  return vim.fn.isdirectory(path) == 1
+end
+
+local valid_workspaces = {}
+local potential_workspaces = {
+  {
+    name = "personal",
+    path = "~/nai_obsidian",
+  },
+  {
+    name = "work",
+    path = "/Users/n.pujol-mendez/Projects/personal/nai_obsidian",
+  },
+}
+
+for _, workspace in ipairs(potential_workspaces) do
+  if workspace_exists(workspace.path) then
+    table.insert(valid_workspaces, workspace)
+  end
+end
+
 return {
   "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  version = "*",
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/nai_obsidian",
-      },
-      -- {
-      --   name = "work",
-      --   path = "~/vaults/work",
-      -- },
-    },
-
-    -- see below for full list of options 👇
+    workspaces = valid_workspaces,
   },
 }
