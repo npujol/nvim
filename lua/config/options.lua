e867df8b2d4aa0260e2f52bf4ad55efee95f8c34
n.pujol-mendez <n.pujol-mendez@mobilistics.de>
2025-09-28 22:44:13 +0200

    chore: configure lazy

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
+vim.g.autoformat = true
+
+local opt = vim.opt
+
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 2                                    -- Hide * markup for bold and italic, but not markers with substitutions
+opt.confirm = true                                      -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true                                   -- Enable highlighting of the current line
+opt.expandtab = true                                    -- Use spaces instead of tabs
+opt.linebreak = true
+
+-- Markdown settings
+vim.g.markdown_recommended_style = 0
