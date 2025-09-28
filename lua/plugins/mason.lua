6e7a90b06f79fec8771814e0e48c6c71a98bb85c
n.pujol-mendez <n.pujol-mendez@mobilistics.de>
2025-09-28 22:44:41 +0200

    chore: update extension name

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,6 +10,6 @@ return {
   --     },
   --   },
   -- },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim",           enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim",           enabled = false },
 }
